5ec3f40a475b313ffc3fd57c10f28b201ddd657b
acanya <arnau_canya@hotmail.com>
2014-02-19 13:51:34 +0100

    edificar
    començats els canvis, pendent d'acabar

--- codi.js
+++ codi.js
@@ -267,7 +267,32 @@ function tipuscasella(jugador){
 	}
 }
 
-//************
+//************ pendent d'acabar d modificar -- Arnau
+function casellasedificables(jugador, i){
+	if (tauler[i+1].propietari == jugador && tauler[i+3].propietari == jugador &&
+		&& tauler[i+1].level <= 2 && tauler[i+1].preuedif >= jugadors[jugador].mon){
+		if (volsedificar[jugador]()/*casella i+1*/ && tauler[i+1].level == tauler[i+3].level){
+			
+		}
+	}
+// falta acabar
+/*	if (tauler[a].propietari == jugador && tauler[b].propietari == jugador
+		&& tauler[c].propietari == jugador && tauler[c].level < 5
+		&& preuhotel(c) <= jugadors[jugador].mon){
+		var edificable = a;
+		if (tauler[b].level < tauler[a].level) edificable = b;
+		if (tauler[c].level < tauler[b].level) edificable = c;
+		if (confirm("Jugador " + jugador + ". Vols edificar a la casella " + edificable + "? Preu: " + preuhotel(c))) {
+			jugadors[jugador].mon -= preuhotel(c);
+			tauler[edificable].level++;
+			casellasedificables(jugador,a,b,c);
+		}
+	}
+}*/
+
+function edificar(jugador){
+	for (var i=0; i<6; ++i) casellesedificables(jugador, i);
+}
 // Construeix edificis si es pot i volsedificar[jugador]
 function edificar(jugador){
 	var shaedificat = false;
