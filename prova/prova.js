a65b1499a4b35413cfd16497cecba68c6bfe4c70
jmigual <j.marce.igual@gmail.com>
2014-02-21 13:38:16 +0100

    Canvis de l'html i css per afegir canvas Treure la foto i poasr la meittat de la pantalla per al dibuix, a l'altre costat hi han d'anar controls. També s'ha modificat l'arxiu prova al realitzar proves de funcions i passar per referència

--- prova/prova.js
+++ prova/prova.js
@@ -1,7 +1,10 @@
-var hola = new Array(3);
-hola[0] = function (pere) {
-	if (pere == 3) return true;
-	return false;
-};
-
-for (var i = 0; i < 10; ++i) console.log(hola[0](i));
+function canvia() {
+	num = 5;
+}
+var num = 3;
+function cosa() {
+	
+	console.log(num);
+	canvia();
+	console.log(num);
+}
