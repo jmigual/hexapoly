eba23334d450640c3e04d3e4d37c1615a277d430
jmigual <j.marce.igual@gmail.com>
2014-03-03 12:55:37 +0100

    Proves del servidor

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var port = 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000);
+app.listen(port);
+
+console.log("Servidor funcionant al port " + port);
